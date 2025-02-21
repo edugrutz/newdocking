fbb798ea1bc16350e05eb051f61dd30c2454af64
eduardo <grutzmann9@gmail.com>
2025-02-21 10:03:46 -0300

    before types

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,16 @@
 import {app, BrowserWindow} from "electron"
 import path from "path"
 import {isDev} from "./util.js"
+import { getPreloadPath } from "./pathResolver.js";
 
 type test = string;
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: getPreloadPath(),
+        },
+    });
     if (isDev()) {
         mainWindow.loadURL("http://localhost:5123");
     } else {
