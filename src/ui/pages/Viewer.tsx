6a3bc571fd3bfd67d33604137312332de2e5d9c2
eduardo <grutzmann9@gmail.com>
2025-03-05 20:31:03 -0300

    custom viewer

--- src/ui/pages/Viewer.tsx
+++ src/ui/pages/Viewer.tsx
@@ -1,22 +1,31 @@
 import { useRef, useEffect, useState } from 'react';
 import * as $3Dmol from '3dmol';
+import ViewerCustom from './ViewerCustom';
 
 const Viewer = ({molViewer, molFormat, molName}) => {
   const viewerContainerRef = useRef(null);
   const viewerRef = useRef(null);
   const [darkMode, setDarkMode] = useState(true);
 
+  const [refresh, setRefresh] = useState(0);
+
   useEffect(() => {
     if (viewerContainerRef.current) {
       viewerRef.current = $3Dmol.createViewer(viewerContainerRef.current, { backgroundColor: 'black' });
       viewerRef.current.addModel(molViewer, molFormat);
       if (molFormat === 'pdb') {
-        viewerRef.current.setStyle({}, { cartoon: { color: 'spectrum' } });
+        viewerRef.current.setStyle({}, { cartoon: {color:'spectrum'} });
       }
       viewerRef.current.zoomTo();
       viewerRef.current.render();
     }
   }, [molViewer]);
+
+  useEffect(() => {
+    if (viewerRef.current) {
+      viewerRef.current.render();
+    }
+  }, [refresh]);
 
   return (
     <div>
@@ -28,6 +37,7 @@ const Viewer = ({molViewer, molFormat, molName}) => {
           <button className='btn btn-sm border border-secondary text-secondary mb-2' onClick={() => {viewerRef.current.setBackgroundColor('black'); setDarkMode(true)}} style={{position:'absolute', top:'10px', left:'10px', zIndex: 1}}><i className="bi bi-circle"></i></button>
         }
         <div ref={viewerContainerRef} style={{ width: '100%', height: '425px', position: 'relative' }}></div>
+      <ViewerCustom viewer={viewerRef} refreshViewer={() => setRefresh(prev => prev + 1)}/>
       </div>
     </div>
   );
---
+++ src/ui/pages/ViewerCustom.tsx
@@ -0,0 +1,63 @@
+import { SurfaceType } from '3dmol';
+import { MeshDoubleLambertMaterial } from '3dmol/build/types/WebGL';
+import React from 'react'
+
+const ViewerCustom = ({viewer, refreshViewer}) => {
+
+    const [surface, setSurface] = React.useState(false);
+    const [style, setStyle] = React.useState(false);
+    const [white, setWhite] = React.useState(false);
+
+  return (
+    <div className='d-flex'>
+        <div>
+            <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => {viewer.current.zoom(1.1)}}><i className="bi bi-zoom-in"></i></button>
+            <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => viewer.current.zoom(0.9)}><i className="bi bi-zoom-out"></i></button>
+        </div>
+        <div>
+          <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => viewer.current.rotate(5)}><i className="bi bi-arrow-clockwise"></i></button>
+          <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => viewer.current.rotate(-5)}><i className="bi bi-arrow-counterclockwise"></i></button>
+          <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => viewer.current.rotate(5, 'x')}><i className="bi bi-arrow-up"></i></button>
+          <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => viewer.current.rotate(-5, 'x')}><i className="bi bi-arrow-down"></i></button>
+        </div>  
+        <div className='ms-auto'>
+            <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => {
+                if (style) {
+                    viewer.current.setStyle({stick: {}}); 
+                    setStyle(false);
+                }
+                else {
+                    viewer.current.setStyle({cartoon: {color:'white'}});
+                    setWhite(true);
+                    setStyle(true);
+                }
+                refreshViewer();
+            }}>Stick</button>
+            <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => {
+                if (surface) {
+                    viewer.current.removeAllSurfaces();
+                    setSurface(false);
+                }
+                else {
+                    viewer.current.addSurface(SurfaceType.VDW, {opacity:0.85});
+                    setSurface(true);
+                }
+                refreshViewer();
+            }}>Surface</button>
+            <button className='btn btn-sm border border-secondary text-secondary mt-2' onClick={() => {
+                if (white) {
+                    viewer.current.setStyle({cartoon: {color: 'spectrum'}}); 
+                    setWhite(false);
+                }
+                else {
+                    viewer.current.setStyle({cartoon: {color: 'white'}});
+                    setWhite(true);
+                }
+                refreshViewer();
+            }}>Color</button>
+        </div>
+    </div>
+  )
+}
+
+export default ViewerCustom
