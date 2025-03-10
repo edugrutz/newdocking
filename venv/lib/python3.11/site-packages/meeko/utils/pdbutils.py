acf486d0e29b4033fd33b34c663760e828857b9d
Eduardo Grutzmann Furtado <127959228+edugrutz@users.noreply.github.com>
2025-03-10 12:41:28 -0300

    Delete venv directory

--- venv/lib/python3.11/site-packages/meeko/utils/pdbutils.py
+++
@@ -1,5 +0,0 @@
-from collections import namedtuple
-
-# named tuple to contain information about an atom
-PDBAtomInfo = namedtuple("PDBAtomInfo", "name resName resNum icode chain")
-PDBResInfo = namedtuple("PDBResInfo", "resName resNum chain")  # used in obutils, maybe
